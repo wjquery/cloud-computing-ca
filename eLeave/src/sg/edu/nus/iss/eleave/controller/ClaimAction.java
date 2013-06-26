8d8bc55a38cd54f93d5639a46bccf81de747a239
xiong.gaogao@gmail.com <xiong.gaogao@gmail.com@4257d849-6a5e-5074-a37a-d55f59f3ed4c>
2013-06-26 09:20:50 +0000

    initial add

---
+++ eLeave/src/sg/edu/nus/iss/eleave/controller/ClaimAction.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.iss.eleave.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Controller
+public class ClaimAction extends ActionSupport {
+
+}
---
+++ eLeave/src/sg/edu/nus/iss/eleave/dto/Employee.java
@@ -0,0 +1,5 @@
+package sg.edu.nus.iss.eleave.dto;
+
+public class Employee {
+
+}
