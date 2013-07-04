1510a7365bcb3f78d8a74de65b76830c48908791
xiong.gaogao@gmail.com <xiong.gaogao@gmail.com@4257d849-6a5e-5074-a37a-d55f59f3ed4c>
2013-07-04 13:24:18 +0000

    test folder added

---
+++ trunk/eLeave/src/sg/edu/nus/iss/eleave/controller/TestData.java
@@ -0,0 +1,44 @@
+package sg.edu.nus.iss.eleave.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestData
+ */
+public class TestData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestData() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
